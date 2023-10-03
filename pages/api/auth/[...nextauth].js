965baa3223fff498c09a6a4e87da55108f6ef8c3
ZukoHeiwa <zukoheiwagithub@gmail.com>
2023-10-04 01:56:41 +0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,11 +9,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
-      version: '2.0', // opt-in to Twitter OAuth 2.0
-    }),
     // ...add more providers here
   ],
 })
